bd997a2500845876ed0f12931f9dae45192d2a30
Juliana R. Mancera <mancerajuliana98@gmail.com>
2024-11-24 23:51:52 +0800

    OP

--- NewsAgency.java
+++ NewsAgency.java
@@ -1,29 +1,54 @@
 import java.util.*;
-
 public class NewsAgency implements NewsSubject {
 
+    private String newsTitle;
+    private String newsContent;
     private List<Subscriber> subscriberList;
 
     public NewsAgency() {
         this.subscriberList = new ArrayList<>();
     }
 
+    // Getters and Setters for newsTitle and newsContent
+    public String getNewsTitle() {
+        return newsTitle;
+    }
+
+    public void setNewsTitle(String newsTitle) {
+        this.newsTitle = newsTitle;
+    }
+
+    public String getNewsContent() {
+        return newsContent;
+    }
+
+    public void setNewsContent(String newsContent) {
+        this.newsContent = newsContent;
+    }
+
+    public List<Subscriber> getSubscriberList() {
+        return subscriberList;
+    }
+
+    public void setSubscriberList(List<Subscriber> subscriberList) {
+        this.subscriberList = subscriberList;
+    }
+
     @Override
     public void attach(Subscriber subscriber) {
         subscriberList.add(subscriber);
-        System.out.println(subscriber.getSubscriberName() + " has subscribed.");
     }
 
     @Override
     public void detach(Subscriber subscriber) {
         subscriberList.remove(subscriber);
-        System.out.println("\n" + subscriber.getSubscriberName() + " has unsubscribed.");
     }
 
     @Override
     public void notifying(String news) {
-        for (SubscriberObserver subscriber : subscriberList) {
-            subscriber.updateNews(news);
+        this.newsContent = news; // Update the news content
+        for (Subscriber subscriber : subscriberList) {
+            subscriber.updateNews(newsTitle + ": " + newsContent);
         }
     }
 }
--- NewsService.java
+++ NewsService.java
@@ -1,9 +1,11 @@
+import java.util.*;
 
 public class NewsService {
 
     public static void main(String[] args) {
 
         NewsAgency agency = new NewsAgency();
+        agency.setSubscriberList(new ArrayList<>());
 
         Subscriber hyunjin = new Subscriber();
         hyunjin.setSubscriberName("Hyunjin");
@@ -18,12 +20,14 @@ public class NewsService {
         agency.attach(bangchan);
 
         System.out.println("\nPublishing Breaking News...");
-        agency.notifying("Breaking: SKZ dominATE concert on Nov. 23, 2024!");
+        agency.setNewsTitle("SKZ dominATE concert on Nov. 23, 2024");
+        agency.notifying("\nDon't miss it! The most awaited concert is here.\n");
 
         agency.detach(bangchan);
         agency.attach(han);
 
         System.out.println("\nPublishing Another News...");
-        agency.notifying("Breaking: The SKZ dominATE concert starts @7pm");
+        agency.setNewsTitle("SKZ dominATE concert starts @7pm");
+        agency.notifying("\nGet ready for an dominATE night of performances!\n");
     }
 }
